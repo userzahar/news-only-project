0d15efaa35881a1972fe2df47d264048bf8c3cee
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-18 12:09:42 +0000

    Deploying to gh-pages from @ userzahar/news-only-project@a1a693abea41fca278d6afd8cb4f12bae0a0ac34 🚀

--- favorite.c77a3c93.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={item:document.querySelectorAll(".gallery__item"),galleryList:document.querySelector(".gallery__list")};var t=window.matchMedia("screen and (min-width: 768px) and (max-width: 1200px)");console.log(t),t.addEventListener("change",(function(t){t.matches?e.item.forEach((function(e){e.classList.add("gallery__item-tablet"),e.classList.remove("gallery__item-desktop")})):e.item.forEach((function(e){e.classList.remove("gallery__item-tablet"),e.classList.add("gallery__item-desktop")}))}))}();
-//# sourceMappingURL=favorite.c77a3c93.js.map
