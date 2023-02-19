2c7d071c2b1902548de7122b02e15566e656f128
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-19 20:15:31 +0200

    rgre

--- src/js/news-page.js
+++ src/js/news-page.js
@@ -26,94 +26,85 @@ async function fetchNews(request) {
   return response.json();
 }
 
-
 function createMarkup(arr, page) {
+  srartIndex = (page - 1) * itemsPerPage;
+  endIndex = srartIndex + itemsPerPage;
+  initPagination(totalPages);
 
-    srartIndex = (page - 1) * itemsPerPage;
-    endIndex = srartIndex + itemsPerPage;
-    initPagination(totalPages);
-
-    const markup = arr.map((el) => {
-        return `<li class="gallery__item">
-
+  const markup = arr.map(el => {
+    return `<li class="gallery__item">
                     <img class="gallery__img" src="${el.image}" alt="${el.alt}"/>
-                    <button class="gallery__favorite">Add to favorite <svg width="16" height="16">
-                    <use href="../images/sprite.svg#icon-heart"></use></svg></button>
                     <h3 class="gallery__header">${el.title}</h3>
                     <p class="gallery__text">${el.descr}</p>
                     <div class="gallery__item-bottom_wrap">
                         <span class="gallery__date">${el.date}</span>
                         <a href="${el.source}" target="_blank" rel="noreferrer noopener" class="gallery__link">Read more</a>
                     </div>
-                </li>`
-    });
-    const pageMarkup = markup.slice(srartIndex, endIndex);
-    // console.log(pageMarkup);
-    pageMarkup.splice(2, 0, emptyCard);
-    const finishedMkp = pageMarkup.join('');
-    // console.log(finishedMkp);
-    // console.log(markup);
- 
-    galleryRef.insertAdjacentHTML('beforeend', finishedMkp);
-};
-
+                </li>`;
+  });
+  const pageMarkup = markup.slice(srartIndex, endIndex);
+  // console.log(pageMarkup);
+  pageMarkup.splice(2, 0, emptyCard);
+  const finishedMkp = pageMarkup.join('');
+  // console.log(finishedMkp);
+  // console.log(markup);
+  galleryRef.insertAdjacentHTML('beforeend', finishedMkp);
+}
 
 function normalizePop(feed) {
-  const marks = feed
-    .map(el => {
-      function checkoutDescr() {
-        if (el.abstract.length > 120) {
-          return el.abstract.slice(0, 119) + '...';
-        }
-        return el.abstract;
+  const marks = feed.map(el => {
+    function checkoutDescr() {
+      if (el.abstract.length > 120) {
+        return el.abstract.slice(0, 119) + '...';
       }
-      const descr = checkoutDescr();
-      const dateFormat = new Date(el.published_date);
-      const date = new Intl.DateTimeFormat().format(dateFormat);
-      function ckeckoutTit() {
-        if (el.title.length > 50) {
-          return el.title.slice(0, 49) + '...';
-        }
-        return el.title;
+      return el.abstract;
+    }
+    const descr = checkoutDescr();
+    const dateFormat = new Date(el.published_date);
+    const date = new Intl.DateTimeFormat().format(dateFormat);
+    function ckeckoutTit() {
+      if (el.title.length > 50) {
+        return el.title.slice(0, 49) + '...';
       }
-      const title = ckeckoutTit();
-      // console.log(title.length);
-      const source = el.url;
-      function checkoutImg() {
-        if (el.media.length === 0) {
-          return 'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg';
-        }
-        return el.media[0]['media-metadata'][2].url;
+      return el.title;
+    }
+    const title = ckeckoutTit();
+    // console.log(title.length);
+    const source = el.url;
+    function checkoutImg() {
+      if (el.media.length === 0) {
+        return 'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg';
       }
-      const image = checkoutImg();
-      function checkoutAlt() {
-        if (el.media.length === 0) {
-          return 'Image is no avalible';
-        }
+      return el.media[0]['media-metadata'][2].url;
+    }
+    const image = checkoutImg();
+    function checkoutAlt() {
+      if (el.media.length === 0) {
+        return 'Image is no avalible';
+      }
+      return el.media[0].caption;
+    }
+    const alt = checkoutAlt();
+    // console.log(alt);
+    // console.log(image);
 
-        const alt = checkoutAlt();
-        // console.log(alt);
-        // console.log(image);
-        
-        return { descr, date, title, source, image, alt };
-    };
-    // console.log(marks);
-    markData = marks;
-    // console.log(markData);
-    return markData;
+    return { descr, date, title, source, image, alt };
+  });
+  // console.log(marks);
+  markData = marks;
+  // console.log(markData);
+  return markData;
 }
 
 function startFetch() {
   fetchNews(reqUrl).then(res => {
-    console.log(res.results);
+    // console.log(res.results);
     totalItems = res.results.length;
     totalPages = Math.ceil(totalItems / itemsPerPage);
     normalizePop(res.results);
     createMarkup(markData, page);
-    });
-    
-};
-
+  });
+}
 startFetch();
 
 function clearMarkup() {
@@ -121,54 +112,53 @@ function clearMarkup() {
 }
 
 function normalizeSrc(feed) {
-    const marks = feed.map(el => {
-         function checkoutDescr() {
-            if (el.abstract.length > 120) {
-                return el.abstract.slice(0, 119) + '...';
-            }
-            return el.abstract;
-        }
-        const descr = checkoutDescr();
-        const dateFormat = new Date(el.pub_date);
-        const date = new Intl.DateTimeFormat().format(dateFormat);
-        function ckeckoutTit() {
-            if (el.headline.main.length > 50) {
-                return el.headline.main.slice(0, 49) + '...';
-            }
-            return el.headline.main;
-        }
-        const title = ckeckoutTit();
-        const source = el.web_url;
-        const imagePart = el.multimedia;
-        
-        function checkoutImg() {
-            if (imagePart.length === 0) {
-                return 'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg';
-            }
-            return `https://static01.nyt.com/${imagePart[0].url}`;
-        }
-        const image = checkoutImg();
-        const alt = 'New`s image';
-        // console.log(image);
-        return { descr, date, title, source, image, alt };
-    });
-    // console.log(marks);
-    markData = marks;
-    return markData;
+  const marks = feed.map(el => {
+    function checkoutDescr() {
+      if (el.abstract.length > 120) {
+        return el.abstract.slice(0, 119) + '...';
+      }
+      return el.abstract;
+    }
+    const descr = checkoutDescr();
+    const dateFormat = new Date(el.pub_date);
+    const date = new Intl.DateTimeFormat().format(dateFormat);
+    function ckeckoutTit() {
+      if (el.headline.main.length > 50) {
+        return el.headline.main.slice(0, 49) + '...';
+      }
+      return el.headline.main;
+    }
+    const title = ckeckoutTit();
+    const source = el.web_url;
+    const imagePart = el.multimedia;
+
+    function checkoutImg() {
+      if (imagePart.length === 0) {
+        return 'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg';
+      }
+      return `https://static01.nyt.com/${imagePart[0].url}`;
+    }
+    const image = checkoutImg();
+    const alt = 'New`s image';
+    // console.log(image);
+    return { descr, date, title, source, image, alt };
+  });
+  // console.log(marks);
+  markData = marks;
+  return markData;
 }
 
 function onSearch(inputData) {
-    const searchUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${inputData}&api-key=${API_KEY}`;
-    fetchNews(searchUrl).then(res => {
-        console.log(res.response.docs);
-        if (res.response.docs.length === 0) {
-            console.log('Empty');
-        }
+  const searchUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${inputData}&api-key=${API_KEY}`;
+  fetchNews(searchUrl).then(res => {
+    console.log(res.response.docs);
+    if (res.response.docs.length === 0) {
+      console.log('Empty');
+    }
     normalizeSrc(res.response.docs);
     createMarkup(markData, page);
-    });
-};
-
+  });
+}
 
 // onSearch('ukraine');
 
@@ -196,29 +186,29 @@ function initPagination(totalPages) {
     // lastItemClassName: 'tui-last-child',
     template: {
       page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton: 
+      currentPage:
+        '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+      moveButton:
         '<a href="#" class="tui-page-btn tui-{{type}}">' +
-          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
         '</a>',
       disabledMoveButton:
         '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
         '</span>',
       moreButton:
         '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-          '<span class="tui-ico-ellip">...</span>' +
-        '</a>'
+        '<span class="tui-ico-ellip">...</span>' +
+        '</a>',
     },
   });
 
   // pagination.disabledMoveButton('first');
 
-  pagination.on('beforeMove', (event) => {
+  pagination.on('beforeMove', event => {
     const currentPage = event.page;
     console.log(event);
     clearMarkup();
     createMarkup(markData, currentPage);
-});
+  });
 }
-
