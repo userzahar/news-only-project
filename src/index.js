ea02f0116dc68d2b6ea7b73adc01e5ca9acba6e7
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-20 23:51:15 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import onResize from './js/resize';
+import { mqHandler } from './js/functions/mqHandler';
 import getCatagories from './js/filter';
 import './js/js-header/dark-mode';
 import './js/js-header/mobile-menu';
-import { mqHandler } from './js/functions/mqHandler';
 import './js/js-read/read';
 import { refs } from './js/refs';
 
