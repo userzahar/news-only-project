b8d82f7c1e2ac14ecbd7a3014d92cc36d6c4d573
talka19 <talka19.10@gmail.com>
2023-02-20 21:43:43 +0200

    markup

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,9 +1,11 @@
-import { refs } from "./refs";
-import { mqHandler } from "./functions/mqHandler";
+import {createMarkup} from './functions/markup';
+import {clearMarkup} from './functions/markup';
+import {markData} from './functions/markup';
+// import {normalizeSrc}  from './functions/markup';
 
-
+const paginationContainer = document.getElementById('pagination');
 function initPagination(totalPages) {
-  const pagination = new tui.Pagination(refs.paginationContainer, {
+  const pagination = new tui.Pagination(paginationContainer, {
     totalItems: totalPages,
     itemsPerPage: 1,
     visiblePages: 3,
@@ -11,8 +13,7 @@ function initPagination(totalPages) {
     pagination.on('beforeMove', event => {
     const currentPage = event.page;
     clearMarkup();
-      createMarkup(markData, currentPage);
-      mqHandler();
+    createMarkup(markData, currentPage);
   });
 }
 export {initPagination};
