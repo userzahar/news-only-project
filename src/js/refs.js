0c56c51a7e0d770829ab1653c525bc84d512a71e
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-21 10:25:11 +0200

    d

--- src/js/refs.js
+++ src/js/refs.js
@@ -7,7 +7,6 @@ export const refs = {
   name: document.querySelector('.catagories__btn-name'),
   catagoriesItem: document.querySelector('.catagories__item-container'),
   item: document.querySelectorAll('.gallery__list li'),
-  galleryList: document.querySelector('.gallery__list'),
   container: document.querySelectorAll('.container'),
   headerContainer: document.querySelectorAll('.header-container'),
   logo: document.querySelector('.logo'),
